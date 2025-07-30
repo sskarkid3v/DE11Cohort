0becd3e0130736896c8ede5cd8d430f45f61e4e3
sskarkid3v <87385074+sskarkid3v@users.noreply.github.com>
2025-07-30 21:56:45 +0545

    Create etl_sekeltion.py

---
+++ Day5_data_modelling/etl_sekeltion.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sqslalchemy import create_engine, text
+import os
+
+#define database connection
+#define data directory
+
+# create dictionary mapping tables to their respective CSV files
+
+#create a function that will create raw tables
+
+#create a function to load csvs into their respective tables
+
+#create a function to implement the kimball model
+# CTAS statements to create the star schema tables
+
+# create a function to implement the medallion architecture
+# create silver and gold tables
+
+#create a function to implement combined kimball and medallion architecture
+
+#create our main function to run the ETL process
